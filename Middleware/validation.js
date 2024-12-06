82636f90a910f2302b310061b75bf27122af1fe2
kaushik <gaudanikaushik945@gmail.com>
2024-12-06 22:47:51 +0530

    change

--- Middleware/validation.js
+++ Middleware/validation.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 // Define the Joi schema for driver validation
 // const driverValidationSchema = Joi.object({
-//   driverName: Joi.string().min(3).max(50).required(),
-//   password: Joi.string().min(6).max(128).reqcjchxjchxjuired(),
+//   driverName: Joi.string().min(3).max(50sfgdkjfskjfsdkjfvdkj).required(),
+//   password: Joi.string().min(6).max(128).required(),
 //   mobileNumber: Joi.number().min(10).required(), 
 //   carModel: Joi.string().min(2).max(50).required(),
 //   rcBookNumber: Joi.string().alphanum().min(5).max(20).required(), 
